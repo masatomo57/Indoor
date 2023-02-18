caff8f5072c3df3cbe80d30a33c04bcafea632f9
xxsatoxx <118036949+xxsatoxx@users.noreply.github.com>
2023-02-18 17:34:58 +0900

    詳しくはREADME.mdに書く。

---
+++ kakeibo/config.py
@@ -0,0 +1,16 @@
+# BaseConfigクラスを作成する
+class BaseConfig:
+    # SECRET_KEYは、自由に変更してOK!
+    SECRET_KEY = "2AZSMss3p5QPbcY2hBsJ"
+    WTF_CSRF_SECRET_KEY = "AuwzyszU5sugKN7KZs6f"
+
+# BaseConfigクラスを継承してLocalConfigクラスを作成する
+class LocalConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{basedir / 'local.sqlite'}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+
+# config辞書にマッピングする
+config = {
+    "local": LocalConfig,
+}
