85a61cdb8279347aa8423d477bd5e5457760db2b
xxsatoxx <118036949+xxsatoxx@users.noreply.github.com>
2023-02-20 04:59:31 +0000

    Mon, Feb 20, 2023, 1:59 PM +09:00

--- kakeibo/app.py
+++ kakeibo/app.py
@@ -71,6 +71,15 @@ def login():
     # Forget any user_id
     session.clear()
 
+    form = LoginForm()
+    if form.validate_on_submit():
+        # process form data
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.email.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
+
+'''
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
@@ -102,7 +111,7 @@ def login():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
-
+'''
 
 @app.route("/logout")
 def logout():
