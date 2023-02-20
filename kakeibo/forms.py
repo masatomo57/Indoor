a16627c9b38ca363c25cc74c79950a55a275abfb
xxsatoxx <118036949+xxsatoxx@users.noreply.github.com>
2023-02-20 05:25:37 +0000

    Mon, Feb 20, 2023, 2:25 PM +09:00

--- kakeibo/forms.py
+++ kakeibo/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField
+from wtforms import PasswordField, StringField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Email, Length
 
 
