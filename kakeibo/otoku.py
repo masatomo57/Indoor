db8ab5ddcab302c4e0f12c455808089ea3121941
masatomo57 <118036554+masatomo57@users.noreply.github.com>
2023-02-26 14:29:19 +0000

    Sun, Feb 26, 2023, 11:29 PM +09:00

--- kakeibo/otoku.py
+++ kakeibo/otoku.py
@@ -1,6 +1,13 @@
 # 毎週水曜日0時に実行する。先週月曜日から日曜日までの購入履歴を取得する。
 import csv
 import sqlite3
+from pandas import read_csv
+
+
+file_name='sample_data.csv'
+df=read_csv(file_name, header=0)
+header=df.columns
+print(header)
 
 
 
@@ -15,7 +22,7 @@ db = conn.cursor()
 users = db.execute("SELECT username FROM users").fetchall()
 
 for username in users:
-    for 
+    for
     last_week_consumptions = db.execute("SELECT item, price, shares FROM buying WHERE user_id=? AND transacted BETWEEN DATE('now', 'localtime', '-9 day') AND DATE('now', 'localtime', '-3 day')", (username[0],)).fetchall()
     print(last_week_consumptions)
 
