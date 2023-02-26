badc14450dcb15e206f10ff5c1175a7a6a8611a4
masatomo57 <118036554+masatomo57@users.noreply.github.com>
2023-02-26 14:16:11 +0000

    Sun, Feb 26, 2023, 11:16 PM +09:00

--- kakeibo/otoku.py
+++ kakeibo/otoku.py
@@ -1,3 +1,4 @@
+# 毎週水曜日0時に実行する。先週月曜日から日曜日までの購入履歴を取得する。
 import csv
 import sqlite3
 
@@ -10,11 +11,11 @@ db = conn.cursor()
 users = db.execute("SELECT username FROM users").fetchall()
 
 for username in users:
-    # 毎週水曜日0時に実行する。先週月曜日から日曜日までの購入履歴を取得する。
-    last_week_consumptions = db.execute("SELECT sum(price*shares) AS sum FROM transact WHERE user_id=? AND transacted BETWEEN DATE('now', 'localtime', '-9 day') AND DATE('now', 'localtime', '-3 day')", (username[0],)).fetchall()[0][0]
-    if last_week_consumptions == None:
-        last_week_consumptions = 0
-    print(last_week_consumptions)
+    last_week_consumption = db.execute("SELECT sum(price*shares) AS sum FROM transact WHERE user_id=? AND transacted BETWEEN DATE('now', 'localtime', '-9 day') AND DATE('now', 'localtime', '-3 day')", (username[0],)).fetchall()[0][0]
+    if last_week_consumption == None:
+        last_week_consumptionn = 0
+    print(last_week_consumption)
+
 
 
 db.close()
