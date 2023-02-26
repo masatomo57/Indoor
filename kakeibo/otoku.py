c85198d3aef87d09f1508f75afa169922d84ee34
masatomo57 <118036554+masatomo57@users.noreply.github.com>
2023-02-26 13:45:40 +0000

    Sun, Feb 26, 2023, 10:45 PM +09:00

--- kakeibo/otoku.py
+++ kakeibo/otoku.py
@@ -1,14 +1,16 @@
 import csv
 import sqlite3
-from app import get_db, read_csv
 
-db = get_db()
+
+
+conn = sqlite3.connect('kakeibo.db')
+db = conn.cursor()
 
 # 毎週水曜日0時に実行する。先週月曜日から日曜日までの購入履歴を取得する。
 users = db.execute("SELECT username FROM users").fetchall()
 
 for username in users:
-    print(users)
+    print(username)
     # last_week_transact = db.execute("SELECT sum(price*shares) AS sum FROM transact WHERE user_id=(?,) transacted BETWEEN DATETIME(CURRENT_DATE, 'localtime', '-9 day') AND DATETIME(CURRENT_DATE, 'localtime', '-3 day')", (username)).fetchall()
 
 
