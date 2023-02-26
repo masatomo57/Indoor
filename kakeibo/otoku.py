4909d00ff51c5c2df819076cb05318e5622a7d4f
masatomo57 <118036554+masatomo57@users.noreply.github.com>
2023-02-26 13:15:28 +0000

    Sun, Feb 26, 2023, 10:15 PM +09:00

--- kakeibo/otoku.py
+++ kakeibo/otoku.py
@@ -2,3 +2,21 @@ import csv
 import sqlite3
 from app import get_db, read_csv
 
+db = get_db()
+if request.method == "POST":
+    name=request.form.get("name")
+    month=request.form.get("month")
+    day=request.form.get("day")
+    db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", (name, month, day))
+    db.commit()
+    return redirect("/")
+else:
+    birthdays = db.execute("SELECT name, month, day FROM birthdays")
+    birthdays = birthdays.fetchall()
+    birthdays_col = ["name", "month", "day"]
+    birthdays_list = []
+    for birthday in birthdays:
+        item = dict(zip(birthdays_col, birthday))
+        birthdays_list.append(item)
+    return render_template("index.html", birthdays=birthdays_list)
+db.close()
