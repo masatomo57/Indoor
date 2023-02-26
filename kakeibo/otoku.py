f8ef718197fd3d417be0b4e2d3e88d540687da4d
masatomo57 <118036554+masatomo57@users.noreply.github.com>
2023-02-26 14:17:29 +0000

    Sun, Feb 26, 2023, 11:17 PM +09:00

--- kakeibo/otoku.py
+++ kakeibo/otoku.py
@@ -13,7 +13,7 @@ users = db.execute("SELECT username FROM users").fetchall()
 for username in users:
     last_week_consumption = db.execute("SELECT sum(price*shares) AS sum FROM transact WHERE user_id=? AND transacted BETWEEN DATE('now', 'localtime', '-9 day') AND DATE('now', 'localtime', '-3 day')", (username[0],)).fetchall()[0][0]
     if last_week_consumption == None:
-        last_week_consumptionn = 0
+        last_week_consumptionn = 00
     print(last_week_consumption)
 
 
