e513667b8db34613419f37257f6bf96dc2ebec90
masatomo57 <118036554+masatomo57@users.noreply.github.com>
2023-02-26 12:54:04 +0000

    Sun, Feb 26, 2023, 9:54 PM +09:00

--- kakeibo/app.py
+++ kakeibo/app.py
@@ -4,7 +4,6 @@ from flask import Flask, render_template, flash, redirect, request, url_for, ses
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
-import datetime
 from helpers import login_required, tax
 import csv
 
