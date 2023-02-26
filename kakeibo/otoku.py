c46a4c3106a9037787b30f0470b6ca842e9c0132
masatomo57 <118036554+masatomo57@users.noreply.github.com>
2023-02-26 13:37:06 +0000

    Sun, Feb 26, 2023, 10:37 PM +09:00

--- kakeibo/otoku.py
+++ kakeibo/otoku.py
@@ -4,23 +4,9 @@ from app import get_db, read_csv
 
 db = get_db()
 
-last_week_transact = db.execute("SELECT * FROM transact WHERE transacted BETWEEN 2023-02-26").fetchall()
+# 毎週水曜日0時に実行する。先週月曜日から日曜日までの購入履歴を取得する。
+last_week_transact = db.execute("SELECT * FROM transact WHERE transacted BETWEEN DATETIME(CURRENT_DATE, 'localtime', '-9 day') AND DATETIME(CURRENT_DATE, 'localtime', '-3 day')").fetchall()
 
-db = get_db()
-if request.method == "POST":
-    name=request.form.get("name")
-    month=request.form.get("month")
-    day=request.form.get("day")
-    db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", (name, month, day))
-    db.commit()
-    return redirect("/")
-else:
-    birthdays = db.execute("SELECT name, month, day FROM birthdays")
-    birthdays = birthdays.fetchall()
-    birthdays_col = ["name", "month", "day"]
-    birthdays_list = []
-    for birthday in birthdays:
-        item = dict(zip(birthdays_col, birthday))
-        birthdays_list.append(item)
-    return render_template("index.html", birthdays=birthdays_list)
+
+
 db.close()
