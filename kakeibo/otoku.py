9a3d82818fcef8e14afa0e59d30a77647cdeb704
masatomo57 <118036554+masatomo57@users.noreply.github.com>
2023-02-26 14:06:53 +0000

    Sun, Feb 26, 2023, 11:06 PM +09:00

--- kakeibo/otoku.py
+++ kakeibo/otoku.py
@@ -11,8 +11,8 @@ users = db.execute("SELECT username FROM users").fetchall()
 
 for username in users:
     # print(username[0])
-    last_week_transact = db.execute("SELECT sum(price*shares) AS sum FROM transact WHERE user_id=? AND transacted BETWEEN DATE('now', 'localtime', '-9 day') AND DATE('now', 'localtime', '-3 day')", (username[0],)).fetchall()
-    
+    last_week_transact = db.execute("SELECT sum(price*shares) AS sum FROM transact WHERE user_id=? AND transacted BETWEEN DATE('now', 'localtime', '-9 day') AND DATE('now', 'localtime', '-3 day')", (username[0],)).fetchall()[0]
+    print(last_week_transact[0])
 
 
 db.close()
