5445568ea8adfd9bc07dba5b14577b9971c7aaee
masatomo57 <118036554+masatomo57@users.noreply.github.com>
2023-02-26 13:43:54 +0000

    Sun, Feb 26, 2023, 10:43 PM +09:00

--- kakeibo/otoku.py
+++ kakeibo/otoku.py
@@ -7,8 +7,9 @@ db = get_db()
 # 毎週水曜日0時に実行する。先週月曜日から日曜日までの購入履歴を取得する。
 users = db.execute("SELECT username FROM users").fetchall()
 
-for user in users:
-    last_week_transact = db.execute("SELECT sum(price*shares) AS sum FROM transact WHERE user_id=(?,) transacted BETWEEN DATETIME(CURRENT_DATE, 'localtime', '-9 day') AND DATETIME(CURRENT_DATE, 'localtime', '-3 day')").fetchall()
+for username in users:
+    print(users)
+    # last_week_transact = db.execute("SELECT sum(price*shares) AS sum FROM transact WHERE user_id=(?,) transacted BETWEEN DATETIME(CURRENT_DATE, 'localtime', '-9 day') AND DATETIME(CURRENT_DATE, 'localtime', '-3 day')", (username)).fetchall()
 
 
 
