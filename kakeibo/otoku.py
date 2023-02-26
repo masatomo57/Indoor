a92fb1f46dcf6fd1f13af7ebb2669adfa6e711e1
masatomo57 <118036554+masatomo57@users.noreply.github.com>
2023-02-26 14:25:45 +0000

    Sun, Feb 26, 2023, 11:25 PM +09:00

--- kakeibo/otoku.py
+++ kakeibo/otoku.py
@@ -15,6 +15,7 @@ db = conn.cursor()
 users = db.execute("SELECT username FROM users").fetchall()
 
 for username in users:
+    for 
     last_week_consumptions = db.execute("SELECT item, price, shares FROM buying WHERE user_id=? AND transacted BETWEEN DATE('now', 'localtime', '-9 day') AND DATE('now', 'localtime', '-3 day')", (username[0],)).fetchall()
     print(last_week_consumptions)
 
